45010884e643531c24ca6780890623048813f8ad
wsb0722 <wsb0722@naver.com>
2020-01-13 03:59:42 +0900

    Create MainController.java

---
+++ simple_board/src/main/java/com/example/simple_board/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.simple_board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/") // method = RequestMethod.POST == @PostMapping
+    public String Index(){
+        return "index";
+    }
+
+}
