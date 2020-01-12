07e1bcee5d1b50c3de3e59016e83812e612e66d4
wsb0722 <wsb0722@naver.com>
2020-01-13 03:52:47 +0900

    Implement  mysql connection And Update project settings
    - Implement  mysql connection through jpa
    - Add thymeleaf setting

--- simple_board/src/main/java/com/example/simple_board/SimpleBoardApplication.java
+++ simple_board/src/main/java/com/example/simple_board/SimpleBoardApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleBoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleBoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBoardApplication.class, args);
+    }
 
 }
